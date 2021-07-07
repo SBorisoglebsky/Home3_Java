91bc179352083daf4b0b416ab2f089912499a46a
SBorisoglebsky <SBorisoglebsky@gmail.com>
2021-07-07 21:20:45 +0300

    add class Cat behavior

--- src/main/java/ga/guru/Main.java
+++ src/main/java/ga/guru/Main.java
@@ -2,6 +2,15 @@ package ga.guru;
 
 public class Main {
     public static void main(String[] args) {
+        Cat barsik = new Cat("Barsik", 2);
+        Cat murky = new Cat("Murka", 3);
+
+        barsik.setGav();
+        murky.runAway();
+
+        System.out.println("Cat number is: "+ Cat.getCatNumber());
+
+        System.out.println("Cat " + barsik.getName() + " is old: "+ barsik.getAge());
 
     }
 }
